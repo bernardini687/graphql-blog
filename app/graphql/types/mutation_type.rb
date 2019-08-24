440a03dc18cb778b1037e6d75d6f566310048a25
Oscar Bernardini <bernardini687@gmail.com>
2019-08-24 19:36:33 +0200

    Add toy mutation for creating a Post

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,10 +1,11 @@
 module Types
   class MutationType < Types::BaseObject
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
+    field :create_post, PostType, null: true do
+      argument :body, String, required: true
+    end
+
+    def create_post(body:)
+      Post.create! body: body, user: User.all.sample
     end
   end
 end
