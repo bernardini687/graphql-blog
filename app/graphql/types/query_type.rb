7320997059c87e4a666ad0238ef3acb1cf2c7edb
Oscar Bernardini <bernardini687@gmail.com>
2019-08-24 10:18:49 +0200

    Serve Post through the API

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,22 +1,30 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    # Root-level fields will be entry points for queries on your schema
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
-    end
-
-    field :user, UserType, null: false do # Or true?
-      description 'Find a user by ID'
+    field :user, UserType, null: true do
+      # description 'This is how to add a description'
       argument :id, ID, required: true
     end
 
-    def user id:
-      User.find_by_id(id) # Or .find
+    field :post, PostType, null: true do
+      argument :id, ID, required: true
     end
+
+    # field :comment, CommentType, null: true do
+    #   argument :id, ID, required: true
+    # end
+
+    def user id:
+      User.find_by_id(id)
+    end
+
+    def post id:
+      Post.find_by_id(id)
+    end
+
+    # def comment id:
+    #   Comment.find_by_id(id)
+    # end
   end
 end
