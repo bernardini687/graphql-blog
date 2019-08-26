4f1cc955274a8a579342decf92f0bd6852ecbb2d
Oscar Bernardini <bernardini687@gmail.com>
2019-08-26 15:32:59 +0200

    Remove type Boolean from BaseDeletion

--- app/graphql/mutations/base_deletion.rb
+++ app/graphql/mutations/base_deletion.rb
@@ -1,9 +1,6 @@
 module Mutations
   class BaseDeletion < GraphQL::Schema::Mutation
     description 'Return `true` if successful or `false` otherwise'
-
     argument :id, ID, required: true
-
-    type Boolean
   end
 end
