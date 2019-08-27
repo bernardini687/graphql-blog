3326feea57df27c0c9b3115aa1e3924afad623c5
Oscar Bernardini <bernardini687@gmail.com>
2019-08-27 09:15:27 +0200

    unit tests for user

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -6,11 +6,31 @@ class CommentTest < ActiveSupport::TestCase
   setup do
     @user = users :one
     @post = posts :one
+    @comment = Comment.new body: 'MyString', user: @user, post: @post
   end
 
-  test "body can't be blank" do
-    comment = Comment.new user: @user, post: @post
+  test 'valid comment' do
+    assert @comment.valid?
+  end
 
-    refute comment.valid?
+  test 'invalid without user' do
+    @comment.user = nil
+
+    refute @comment.valid?
+    assert_not_empty @comment.errors[:user]
+  end
+
+  test 'invalid without post' do
+    @comment.post = nil
+
+    refute @comment.valid?
+    assert_not_empty @comment.errors[:post]
+  end
+
+  test 'invalid without body' do
+    @comment.body = ''
+
+    refute @comment.valid?
+    assert_not_empty @comment.errors[:body]
   end
 end
