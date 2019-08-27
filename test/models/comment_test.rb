2e8f6e04a3ecd0ca07b57a8d64d68f6701c542b3
Oscar Bernardini <bernardini687@gmail.com>
2019-08-27 09:03:24 +0200

    setup fixtures

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,16 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users :one
+    @post = posts :one
+  end
+
+  test "body can't be blank" do
+    comment = Comment.new user: @user, post: @post
+
+    refute comment.valid?
+  end
 end
