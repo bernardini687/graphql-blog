613119c006bb1e1390ed4392e588530bcb84c81d
Oscar Bernardini <bernardini687@gmail.com>
2019-09-27 10:59:27 +0200

    generate session model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,12 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :sessions
 
   validates :first_name, :street, :city, :postcode, :country, presence: true
   validates :number, numericality: { only_integer: true }
+
+  has_secure_password
 
   def success_payload
     { user: self, errors: [] }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_23_160740) do
+ActiveRecord::Schema.define(version: 2019_09_27_085658) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(version: 2019_08_23_160740) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "sessions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "key", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name", null: false
     t.string "last_name"
@@ -45,4 +53,5 @@ ActiveRecord::Schema.define(version: 2019_08_23_160740) do
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
+  add_foreign_key "sessions", "users"
 end
